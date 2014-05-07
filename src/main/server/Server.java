3b1f833c454ef0ad247126d51e022f87654b2a8c
scorpionboy30 <scorpionboy30@users.noreply.github.com>
2014-05-07 16:45:03 -0700

    fileupload

--- src/main/server/Server.java
+++ src/main/server/Server.java
@@ -139,6 +139,22 @@ public class Server implements Runnable {
 	public String sayHelloToServer() {
 		return "Hello World";
 	}
+	
+	public String uploadFile(CommandWrapper command) throws IOException{
+		Value[] argument = command.getArgs();
+		MessagePack msgpack = new MessagePack();
+		byte raw[] = msgpack.write(argument);
+		ByteArrayInputStream in = new ByteArrayInputStream(raw);
+        Unpacker unpacker = msgpack.createUnpacker(in);
+		
+        String bucketname = new Converter(argument[0]).read(Templates.TString);
+        String filename = new Converter(argument[1]).read(Templates.TString);
+        String filepath = new Converter(argument[2]).read(Templates.TString);
+		
+		String reply = FileUploadToS3.UploadToS3(bucketname,filename, filepath);
+		
+		return reply;
+	}
 
 	@Override
 	public void run() {
